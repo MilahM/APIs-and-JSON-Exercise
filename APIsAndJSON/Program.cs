e8ec86aa03ccb1c3415cfd97202e781d08a3f40f
Kamilah <k.muhammad95@gmail.com>
2023-02-25 14:39:41 -0600

    regions added

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -7,30 +7,30 @@ namespace APIsAndJSON
         static void Main(string[] args)
         {
             #region RonVSKanye
-            //var client = new HttpClient();
-            //var quote = new RonVSKanyeAPI(client);
+            var client = new HttpClient();
+            var quote = new RonVSKanyeAPI(client);
 
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    Console.WriteLine("------------------------");
-            //    Console.WriteLine($"Kanye: {quote.KanyeQuote()}");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("------------------------");
+                Console.WriteLine($"Kanye: {quote.KanyeQuote()}");
 
-            //    Console.WriteLine($"Ron Swanson: {quote.RonQuote()}");
-            //}
+                Console.WriteLine($"Ron Swanson: {quote.RonQuote()}");
+            }
             #endregion
 
             #region OpenWeatherMap
-            string key = File.ReadAllText("appsettings.json");
+            //string key = File.ReadAllText("appsettings.json");
 
-            string APIKey = JObject.Parse(key).GetValue("Default key").ToString();
+            //string APIKey = JObject.Parse(key).GetValue("Default key").ToString();
 
-            Console.WriteLine("Please enter your Zip Code.");
-            var zipCode = Console.ReadLine();
+            //Console.WriteLine("Please enter your Zip Code.");
+            //var zipCode = Console.ReadLine();
 
-            string apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={APIKey}&units=imperial";
+            //string apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={APIKey}&units=imperial";
 
-            Console.WriteLine();
-            Console.WriteLine($"The current temperature in your location is {OpenWeatherMapAPI.GetTemp(apiCall)}");
+            //Console.WriteLine();
+            //Console.WriteLine($"The current temperature in your location is {OpenWeatherMapAPI.GetTemp(apiCall)}");
             #endregion
         }
     }
